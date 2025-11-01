4b3deeacd4d92505532b1f2360b02d059b641296
sivakagit <sivaflowx13.com>
2025-11-02 04:03:08 +0530

    "small fixes"

--- client/src/api/axios.ts
+++ client/src/api/axios.ts
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://lpu-sphere-frontend.onrender.com/api",
+  baseURL: "https://lpu-sphere-frontend.onrender.com/",
   headers: { "Content-Type": "application/json" },
 });
 
