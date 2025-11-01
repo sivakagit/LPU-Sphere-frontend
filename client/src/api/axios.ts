ce5eb56e9e15c375869e467aaf558ea653778454
sivakagit <sivaflowx13.com>
2025-11-01 06:10:53 +0530

    vercel

--- client/src/api/axios.ts
+++ client/src/api/axios.ts
@@ -1,8 +1,7 @@
 // src/api/axios.ts
 import axios from "axios";
 
-// 🧠 DEBUG: Check what VITE_API_URL is
-console.log("🌍 VITE_API_URL =", import.meta.env.VITE_API_URL);
+
 
 const api = axios.create({
   baseURL: "https://lpu-sphere-backend.vercel.app/api",
